e12c20474b9a2aa3855e802330278c251571392c
xla27 <alberto.perlini99@gmail.com>
2025-04-22 23:49:08 +0200

    New repository for Polynomial Chaos Expansion

---
+++ main.py
@@ -0,0 +1,106 @@
+import os, sys, shutil
+os.environ["MKL_NUM_THREADS"] = "1" 
+os.environ["NUMEXPR_NUM_THREADS"] = "1" 
+os.environ["OMP_NUM_THREADS"] = "1" 
+os.environ['OPENBLAS_NUM_THREADS'] = "1"
+
+import numpy as np
+from scipy.special import legendre, eval_legendre, hermite, eval_hermite
+from scipy.stats import qmc
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.metrics import r2_score
+
+from itertools import product, repeat
+import time
+
+from pce import PCE
+
+# Ishigami function
+dim = 3
+def Ishigami(x):
+    x1, x2, x3 = x
+    fun = np.sin(x1) + 7 * np.sin(x2)**2 + 0.1 * x3**4 * np.sin(x1)
+    return fun
+bndlw_obj = np.array([-np.pi]*dim)
+bndup_obj = np.array([ np.pi]*dim)
+
+# Design of experiments from Latin Hypercube
+doe = qmc.LatinHypercube(d=dim)
+
+def from_unit_uniform(X, bndlw, bndup):
+    ndata, _ = X.shape
+    bndlw = np.repeat(bndlw[np.newaxis,:], ndata, axis=0)
+    bndup = np.repeat(bndup[np.newaxis,:], ndata, axis=0)
+    X_out = (bndup + bndlw) / 2 + X * (bndup - bndlw) / 2
+    return X_out
+
+def to_unit_uniform(X, bndlw, bndup):
+    ndata, _ = X.shape
+    bndlw = np.repeat(bndlw[np.newaxis,:], ndata, axis=0)
+    bndup = np.repeat(bndup[np.newaxis,:], ndata, axis=0)
+    X_out = (X - (bndup - bndlw)/2) / ((bndup - bndlw)/2)
+    return X_out
+
+def from_unit_gaussian(X, mean, std):
+    ndata, _ = X.shape
+    mean = np.repeat(mean[np.newaxis,:], ndata, axis=0)
+    std = np.repeat(std[np.newaxis,:], ndata, axis=0)
+    X_out = mean + std * X
+    return X_out
+
+def to_unit_gaussian(X, mean, std):
+    ndata, _ = X.shape
+    mean = np.repeat(mean[np.newaxis,:], ndata, axis=0)
+    std = np.repeat(std[np.newaxis,:], ndata, axis=0)
+    X_out = (X - mean) / std
+    return X_out
+
+scaler_uniform  = FunctionTransformer(func=to_unit_uniform,  inverse_func=from_unit_uniform,  kw_args={'bndlw':bndlw_obj, 'bndup':bndup_obj}, inv_kw_args={'bndlw':bndlw_obj, 'bndup':bndup_obj})
+scaler_gaussian = FunctionTransformer(func=to_unit_gaussian, inverse_func=from_unit_gaussian, kw_args={'bndlw':bndlw_obj, 'bndup':bndup_obj}, inv_kw_args={'bndlw':bndlw_obj, 'bndup':bndup_obj})
+
+# PCE construction
+p_vec = range(3, 16)
+
+Si = np.zeros((len(p_vec), dim))
+err = np.zeros(len(p_vec))
+delta_t = np.zeros(len(p_vec))
+
+for i ,p in enumerate(p_vec):
+
+    print('\nDegree = ', p)
+
+    # pce
+    truncation = {'method':'hyperbolic', 'q': 0.5}
+    pce = PCE(dim, degree=p, type='UUU', truncation=truncation)
+
+    # cardinality
+    card = np.math.factorial(dim+p)/(np.math.factorial(dim) * np.math.factorial(p))
+    print('card = ', card)
+
+    # doe
+    ndata = int(3*card)
+    X = doe.random(n=ndata)
+    X = -1 + 2*X    # from [0,1] to [-1,1]
+
+    X_obj = scaler_uniform.inverse_transform(X)
+
+    y = np.zeros(ndata)
+    for j in range(ndata):
+        y[j] = Ishigami(X_obj[j,:])
+
+    time_init = time.time()
+    # coefficients
+    pce.compute_coeffs(X, y, method='LSQ')
+    print(pce.coeffs.shape)
+
+    delta_t[i] = time.time() - time_init
+
+    # Sobol' indices
+    Si[i,:] = pce.sobol_first()
+    print('Si', Si[i,:])
+
+    # Leave-One-Out error
+    err[i] = pce.error_loo()
+    print('err LOO = ', err[i])
+
+
---
+++ pce.py
@@ -0,0 +1,192 @@
+import os, sys, shutil
+
+import numpy as np
+from scipy.special import legendre, hermite
+from scipy.linalg import lstsq, inv
+
+from itertools import product
+
+
+# PCE class
+class PCE():
+
+    pdf_types = ['U', 'N', 'G', 'B']
+
+    def __init__(self, dim, degree, type, truncation):
+
+        self.dim = dim        # size of input domain
+        self.deg = degree     # maximum degree of polynomials
+
+        self.type = type      # pdf of reduced variables (U, N, G, B)
+        if any(t not in self.pdf_types for t in self.type):
+            raise KeyError('Invalide PDF for reduced variables')
+        if len(self.type) != self.dim:
+            raise KeyError('Unspecified PDF kind for input size')
+
+        # truncation scheme
+        if truncation['method'] == 'standard':
+            self.multindices = trunc_standard(self.dim, self.deg)
+        elif truncation['method'] == 'rank':
+            self.multindices = trunc_rank(self.dim, self.deg, truncation['rank'])
+        elif truncation['method'] == 'hyperbolic':
+            self.multindices = trunc_hyper(self.dim, self.deg, truncation['q'])
+
+        # polynomial bases
+        self.polynomials = []
+        for j, indices in enumerate(self.multindices):
+
+            poly = []
+            types = self.type.split()
+
+            for k, idx in enumerate(indices):
+
+                if types[k] == 'U':
+                    poly.append(legendre_norm(idx))
+
+                elif types[k] == 'N':
+                    poly.append(hermite_norm(idx))
+
+                elif types[k] == 'G':
+                    raise KeyError('Laguerre polynomials for Gamma distribution not yet implemented')
+                
+                elif types[k] == 'B':
+                    raise KeyError('Jacobi polynomials for Beta distribution not yet implemented')
+                
+            self.polynomials.append(poly)
+
+        return
+
+    def compute_coeffs(self, X, y, method):
+        """
+        X is a (ndata, dim) numpy.array
+        """
+
+        ndata, _ = X.shape
+
+        self.X_train = X
+        self.y_train = y
+        self.A = np.zeros((ndata, len(self.multindices)))
+
+        if method == 'LSQ':
+
+            for i in range(ndata):
+
+                for j, poly in enumerate(self.polynomials):
+
+                    prod = 1
+                    for k, p in enumerate(poly):
+                        prod *= p(X[i,k])
+                    
+                    self.A[i,j] = prod
+
+            self.coeffs, _, _, _ = lstsq(self.A, self.y_train)
+
+        elif method == 'PROJ':
+
+            raise NotImplementedError('Projection method using quadrature not yet implemented')
+
+    def moments(self):
+
+        if not self.coeffs.any():
+            raise KeyError('Coefficients do not exist! Surrogate yet to be built!')
+
+        mean = self.coeffs[0]
+        var  = np.sum(self.coeffs[1:]**2)
+
+        return mean, var
+    
+    def predict(self, X):
+
+        if not self.coeffs.any():
+            raise KeyError('Coefficients do not exist! Surrogate yet to be built!')
+
+        ndata, _ = X.shape
+        
+        y = np.zeros(ndata)
+        for i in range(ndata):
+
+            for j, poly in enumerate(self.polynomials):
+
+                prod = 1
+                for k, p in enumerate(poly):
+                    prod *= p(X[i,k])
+                
+                y[i] += self.coeffs[j] * prod  
+
+        return y     
+    
+    def sobol_first(self):
+           
+        if not self.coeffs.any():
+            raise KeyError('Coefficients do not exist! Surrogate yet to be built!')    
+
+        _, V = self.moments()
+
+        s = np.zeros(self.dim)
+        for d in range(self.dim):
+
+            for j, indices in enumerate(self.multindices):
+
+                s[d] += self.coeffs[j]**2 if (indices[d] > 0 and np.sum([indices[i] for i in range(self.dim) if i != d]) == 0.0) else 0.0 
+
+        return s / (V + np.finfo(float).eps)
+    
+    def sobol_total(self):
+
+        if not self.coeffs.any():
+            raise KeyError('Coefficients do not exist! Surrogate yet to be built!')    
+
+        _, V = self.moments()
+
+        s = np.zeros(self.dim)
+        for d in range(self.dim):
+
+            for j, indices in enumerate(self.multindices):
+
+                s[d] += self.coeffs[j]**2 if indices[d] > 0 else 0.0 
+
+        return s / (V + np.finfo(float).eps)
+    
+    def error_loo(self):
+
+        h = np.diag(self.A @ inv(self.A.T @ self.A) @ self.A.T)
+
+        ndata, _ = self.X_train.shape
+
+        err_loo = 0.0
+        for i in range(ndata):
+            err_loo += 1/ndata * ((self.y_train[i] - self.predict(self.X_train[i,:].reshape(1,self.dim))) / (1 - h[i]))**2
+
+        return err_loo
+    
+
+######################
+# ORTHONORMAL FUNCTIONS
+
+def legendre_norm(k):
+    return legendre(k) / np.sqrt(1 / (2*k + 1))
+
+def hermite_norm(k):
+    return hermite(k) / np.sqrt(np.math.factorial(k))
+
+
+######################
+# TRUNCATION SCHEMES
+
+def trunc_standard(dim, deg):
+    combinations = list( product( range(0, deg+1), repeat=dim) )
+    multindices = [combo for combo in combinations if np.sum(combo) <= deg]
+    return multindices
+
+def trunc_rank(dim, deg, rank):
+    multindices_standard = trunc_standard(dim, deg)
+    multindices = []
+    for indices in multindices_standard:
+        if np.sum(np.where(np.asarray(indices) > 0, 1, 0)) <= rank:
+            multindices.append(indices)   
+    return multindices
+
+def trunc_hyper(dim, deg, q):
+    combinations = list( product( range(0, deg+1), repeat=dim) )
+    multindices = [combo for combo in combinations if np.linalg.norm(combo, q) <= deg]
+    return multindices
