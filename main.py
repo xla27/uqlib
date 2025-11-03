6e0fcebde6d460b55b36d74723753576c3dd9c7b
xla27 <alberto.perlini99@gmail.com>
2025-11-03 11:21:09 +0100

    Update

--- main.py
+++ main.py
@@ -14,91 +14,63 @@ from itertools import product, repeat
 import time
 
 from pce import PCE
+from doe import DoE, DataScaler
 
 # Ishigami function
 dim = 3
 def Ishigami(x):
-    x1, x2, x3 = x
-    fun = np.sin(x1) + 7 * np.sin(x2)**2 + 0.1 * x3**4 * np.sin(x1)
+    fun = np.sin(x[:,0]) + 7 * np.sin(x[:,1])**2 + 0.1 * x[:,2]**4 * np.sin(x[:,0])
     return fun
-bndlw_obj = np.array([-np.pi]*dim)
-bndup_obj = np.array([ np.pi]*dim)
+bndlw = np.array([-np.pi]*dim)
+bndup = np.array([ np.pi]*dim)
+
+pdf_var = ['U', 'U', 'U']
 
 # Design of experiments from Latin Hypercube
-doe = qmc.LatinHypercube(d=dim)
+doe = DoE(dim, method='QUADRATURE', pdf_var=pdf_var)
 
-def from_unit_uniform(X, bndlw, bndup):
-    ndata, _ = X.shape
-    bndlw = np.repeat(bndlw[np.newaxis,:], ndata, axis=0)
-    bndup = np.repeat(bndup[np.newaxis,:], ndata, axis=0)
-    X_out = (bndup + bndlw) / 2 + X * (bndup - bndlw) / 2
-    return X_out
+X, w = doe(point_per_dim=8)
+ndata, dim = X.shape
 
-def to_unit_uniform(X, bndlw, bndup):
-    ndata, _ = X.shape
-    bndlw = np.repeat(bndlw[np.newaxis,:], ndata, axis=0)
-    bndup = np.repeat(bndup[np.newaxis,:], ndata, axis=0)
-    X_out = (X - (bndup - bndlw)/2) / ((bndup - bndlw)/2)
-    return X_out
-
-def from_unit_gaussian(X, mean, std):
-    ndata, _ = X.shape
-    mean = np.repeat(mean[np.newaxis,:], ndata, axis=0)
-    std = np.repeat(std[np.newaxis,:], ndata, axis=0)
-    X_out = mean + std * X
-    return X_out
-
-def to_unit_gaussian(X, mean, std):
-    ndata, _ = X.shape
-    mean = np.repeat(mean[np.newaxis,:], ndata, axis=0)
-    std = np.repeat(std[np.newaxis,:], ndata, axis=0)
-    X_out = (X - mean) / std
-    return X_out
-
-scaler_uniform  = FunctionTransformer(func=to_unit_uniform,  inverse_func=from_unit_uniform,  kw_args={'bndlw':bndlw_obj, 'bndup':bndup_obj}, inv_kw_args={'bndlw':bndlw_obj, 'bndup':bndup_obj})
-scaler_gaussian = FunctionTransformer(func=to_unit_gaussian, inverse_func=from_unit_gaussian, kw_args={'bndlw':bndlw_obj, 'bndup':bndup_obj}, inv_kw_args={'bndlw':bndlw_obj, 'bndup':bndup_obj})
+scaler = DataScaler(pdf_var, bndlw, bndup)
 
 # PCE construction
-p_vec = range(3, 16)
+p = 6
 
-Si = np.zeros((len(p_vec), dim))
-err = np.zeros(len(p_vec))
-delta_t = np.zeros(len(p_vec))
+print('\nDegree = ', p)
 
-for i ,p in enumerate(p_vec):
+# pce
+truncation = {'method':'standard'}
+pce1 = PCE(dim, degree=p, pdf_var=pdf_var, truncation=truncation)
+pce2 = PCE(dim, degree=p, pdf_var=pdf_var, truncation=truncation)
 
-    print('\nDegree = ', p)
+# cardinality
+card = math.factorial(dim+p)/(math.factorial(dim) * math.factorial(p))
+print('card = ', card, '\tndata = ', ndata)
 
-    # pce
-    truncation = {'method':'hyperbolic', 'q': 0.5}
-    pce = PCE(dim, degree=p, type='UUU', truncation=truncation)
+# ydata
+X_obj = scaler.inverse_transform(X)
 
-    # cardinality
-    card = math.factorial(dim+p)/(math.factorial(dim) * math.factorial(p))
-    print('card = ', card)
+y = Ishigami(X_obj)
 
-    # doe
-    ndata = int(3*card)
-    X = doe.random(n=ndata)
-    X = -1 + 2*X    # from [0,1] to [-1,1]
+time_init = time.time()
+# coefficients
+pce1.compute_coeffs(X, y, method='LSQ')
+pce2.compute_coeffs(X, y, method='PROJ', weights=w)
 
-    X_obj = scaler_uniform.inverse_transform(X)
+# Sobol' indices
+print('Si LSQ', pce1.sobol_first())
+print('Si PROJ', pce2.sobol_first())
 
-    y = np.zeros(ndata)
-    for j in range(ndata):
-        y[j] = Ishigami(X_obj[j,:])
+# scoring the two surrogates on test points
+ntest = 1000
+xtest = np.random.uniform(-np.pi, np.pi, size=(ntest,dim))
 
-    time_init = time.time()
-    # coefficients
-    pce.compute_coeffs(X, y, method='LSQ')
-    print(pce.coeffs.shape)
+ytest = Ishigami(xtest)
+ypred1 = pce1.predict(scaler.transform(xtest))
+ypred2 = pce2.predict(scaler.transform(xtest))
 
-    # Sobol' indices
-    Si[i,:] = pce.sobol_first()
-    print('Si', Si[i,:])
-
-    # Leave-One-Out error
-    err[i] = pce.compute_err_mse()
-    print('err MSE = ', err[i])
-
+from sklearn.metrics import r2_score
+print('R2 PCE 1 = ', r2_score(ytest, ypred1))
+print('R2 PCE 2 = ', r2_score(ytest, ypred2))
 
