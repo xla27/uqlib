5ccd74783478f908d9223e52502a01537ba9a0b9
xla27 <alberto.perlini99@gmail.com>
2025-11-17 12:16:01 +0100

    New test function

--- main.py
+++ main.py
@@ -16,61 +16,89 @@ import time
 from pce import PCE
 from doe import DoE, DataScaler
 
-# Ishigami function
-dim = 3
-def Ishigami(x):
-    fun = np.sin(x[:,0]) + 7 * np.sin(x[:,1])**2 + 0.1 * x[:,2]**4 * np.sin(x[:,0])
-    return fun
-bndlw = np.array([-np.pi]*dim)
-bndup = np.array([ np.pi]*dim)
+PROBLEM = input('Problem: ')
+np.set_printoptions(precision=4,threshold=sys.maxsize)
 
-pdf_var = ['U', 'U', 'U']
+# Ishigami function
+def Ishigami():
+    fun = lambda x: np.sin(x[:,0]) + 7 * np.sin(x[:,1])**2 + 0.1 * x[:,2]**4 * np.sin(x[:,0])
+    dim = 3
+    bndlw = np.array([-np.pi]*dim)
+    bndup = np.array([ np.pi]*dim)
+    V_i = np.array([0.3138, 0.4424, 0.0])
+    return fun, dim, bndlw, bndup, V_i
+
+# Sobol G function 
+def Sobol_G():
+    a = np.array([0, 1, 4.5, 9, 99, 99, 99, 99])
+    dim = 8
+    def fun(x):
+        a = np.array([0, 1, 4.5, 9, 99, 99, 99, 99])
+        a = np.repeat(a[np.newaxis,:],repeats=x.shape[0],axis=0)
+        return np.prod((np.abs(4*x - 2) + a)/(1+a), axis=1)
+    bndlw = np.array([0]*dim)
+    bndup = np.array([1]*dim)
+    V_i = np.array([1 / (3 * (1 + a[i])**2) for i in range(dim)])
+    V = np.prod(V_i+1)-1
+    return fun, dim, bndlw, bndup, V_i, V
+
+if PROBLEM == 'Ishigami':
+    obj, dim, bndlw, bndup, V_i = Ishigami()
+    pdf_var = ['U']*dim
+
+elif PROBLEM == 'Sobol G':
+    obj, dim, bndlw, bndup, V_i, V = Sobol_G() 
+    pdf_var = ['U']*dim
+
 
 # Design of experiments from Latin Hypercube
-doe = DoE(dim, method='QUADRATURE', pdf_var=pdf_var)
-
-X, w = doe(point_per_dim=8)
-ndata, dim = X.shape
+doe = DoE(dim, method='MC', pdf_var=pdf_var)
 
 scaler = DataScaler(pdf_var, bndlw, bndup)
 
-# PCE construction
-p = 6
+for p in range(3, 15):
+    # PCE construction
 
-print('\nDegree = ', p)
+    print('\nDegree = ', p)
 
-# pce
-truncation = {'method':'standard'}
-pce1 = PCE(dim, degree=p, pdf_var=pdf_var, truncation=truncation)
-pce2 = PCE(dim, degree=p, pdf_var=pdf_var, truncation=truncation)
+    # pce
+    truncation = {'method':'hyperbolic', 'q': 0.75}
+    pce = PCE(dim, degree=p, pdf_var=pdf_var, truncation=truncation)
 
-# cardinality
-card = math.factorial(dim+p)/(math.factorial(dim) * math.factorial(p))
-print('card = ', card, '\tndata = ', ndata)
+    # cardinality
+    card = math.factorial(dim+p)/(math.factorial(dim) * math.factorial(p))
+    ndata= int(3*card)
+    print('card = ', card, '\tndata = ', ndata)
 
-# ydata
-X_obj = scaler.inverse_transform(X)
+    # ydata
+    X, w = doe(ndata=ndata)
+    ndata, dim = X.shape
+    X_obj = scaler.inverse_transform(X)
 
-y = Ishigami(X_obj)
+    y = obj(X_obj)
 
-time_init = time.time()
-# coefficients
-pce1.compute_coeffs(X, y, method='LSQ')
-pce2.compute_coeffs(X, y, method='PROJ', weights=w)
+    time_init = time.time()
+    # coefficients
+    pce.compute_coeffs(X, y, method='LSQ')
 
-# Sobol' indices
-print('Si LSQ', pce1.sobol_first())
-print('Si PROJ', pce2.sobol_first())
+    # Moments
+    if PROBLEM == 'Sobol G':
+        _, var = pce.moments()
+        print('Variance', var)
+        print('Exact V', V)
 
-# scoring the two surrogates on test points
-ntest = 1000
-xtest = np.random.uniform(-np.pi, np.pi, size=(ntest,dim))
+    # Sobol' indices
+    print('Si  LSQ', pce.sobol_first())
+    print('Exact Si', V_i)
+    print('Sij LSQ', pce.sobol_second())
 
-ytest = Ishigami(xtest)
-ypred1 = pce1.predict(scaler.transform(xtest))
-ypred2 = pce2.predict(scaler.transform(xtest))
+    # scoring the two surrogates on test points
+    ntest = 1000
+    xtest = scaler.inverse_transform(doe(ndata=ntest)[0])
 
-from sklearn.metrics import r2_score
-print('R2 PCE 1 = ', r2_score(ytest, ypred1))
-print('R2 PCE 2 = ', r2_score(ytest, ypred2))
+    ytest = obj(xtest)
+    ypred = pce.predict(scaler.transform(xtest))
+
+    from sklearn.metrics import r2_score
+    print('R2 PCE = ', r2_score(ytest, ypred))
 
