a650172c302ff3cfc028312629c073bb12376e7a
xla27 <alberto.perlini99@gmail.com>
2025-11-28 17:45:29 +0100

    Updates on isomap

--- isomap.py
+++ isomap.py
@@ -1,14 +1,16 @@
 import os, sys, shutil
 import numpy as np
 from scipy.linalg import svd, eigh
-from scipy.spatial.distance import pdist, cdist
-from scipy.optimize import minimize, LinearConstraint, Bounds
+from scipy.spatial.distance import pdist, cdist, squareform
+from scipy.optimize import minimize, LinearConstraint, Bounds, fmin_slsqp
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import shortest_path
 from sklearn.neighbors import NearestNeighbors
 import copy
 
 from . import PCE
+
+np.set_printoptions(threshold=sys.maxsize)
 
 class ISOMAPPCE():
     '''
@@ -22,12 +24,11 @@ class ISOMAPPCE():
 
         self.dim     = dim
         self.pdf_var = pdf_var
-        self.pce_parameters = {'dim':        dim,
-                               'degree':     degree,
-                               'pdf_var':    pdf_var,
-                               'truncation': truncation}
+
+        # building a PCE object
+        self.pce = PCE(dim, degree, pdf_var, truncation)
         
-    def compute_isomap(self, Y, delta=0.9999):
+    def compute_isomap(self, Y, delta=0.9999, distance='geodesic'):
         '''
         Z is an (M x N) array with:
         - M number of snaphsots
@@ -36,59 +37,79 @@ class ISOMAPPCE():
         Y = np.asarray(Y)
         if Y.ndim != 2:
             raise ValueError("Y must be 2D array of shape (M, N)")
-        else:
-            self.snapshots = Y
-
-        self.snapshots = Y
+        
         self.ndata, self.noutputs = Y.shape
 
-        #self.k_near = k_near
+        # removing the mean
+        #self.Y_mean = np.mean(Y, axis=0)
+        snapshots = Y #- np.repeat(self.Y_mean[np.newaxis,:], self.ndata, axis=0)
 
         # looping to finde the optimal k_near
-        k_list = [k for k in range(10, 60, 5)]
+        k_max = max(int(np.log(self.ndata) * 10) , self.ndata-1)
+        k_list = [k for k in range(1, k_max)]
         kruskal_list = []
         eigvals_list = []
         eigvecs_list = []
 
         for i_k, k_near in enumerate(k_list):
 
-            # computing the dissimilarity matrix
-            D = self._compute_dissimilarity(Y, k_near)
+            try:
 
-            # computing D_tilde
-            D_tilde = 0.5 * D**2
+                # computing the dissimilarity matrix
+                if distance == 'geodesic':
+                    D = self._compute_dissimilarity(snapshots, k_near)
+                elif distance == 'euclidean':
+                    D = squareform(pdist(snapshots, metric='euclidean'))
 
-            # removing row-wise and column-wise mean from D_tilde
-            H = np.eye(self.ndata) - 1/self.ndata * np.ones((self.ndata, self.ndata))
-            B = H @ D_tilde @ H
+                # computing D_tilde
+                D_tilde = 0.5 * D**2
 
-            # eigendecomposing B
-            eigvals, eigvecs = eigh(B, subset_by_value=[0.0, np.inf], driver='evr')
-            Z_tilde = eigvecs @ np.diag(np.sqrt(eigvals))
+                # removing row-wise and column-wise mean from D_tilde
+                H = np.eye(self.ndata) - 1/self.ndata * np.ones((self.ndata, self.ndata))
+                B = H @ D_tilde @ H
 
-            # computing the Kruskal's stress (for optimum k)
-            kruskal = self._kruskal_stress(D, Z_tilde)
+                # eigendecomposing B
+                eigvals_k, eigvecs_k = eigh(B, subset_by_value=[0.0, np.inf], driver='evr')
 
-            kruskal_list.append(kruskal)
-            eigvals_list.append(eigvals)
-            eigvecs_list.append(eigvecs)
+                Z_tilde = eigvecs_k @ np.diag(np.sqrt(eigvals_k))
+
+                # computing the Kruskal's stress (for optimum k)
+                kruskal = self._kruskal_stress(D, Z_tilde)
+
+                kruskal_list.append(kruskal)
+                eigvals_list.append(eigvals_k)
+                eigvecs_list.append(eigvecs_k)
+
+            except ValueError as e:
+
+                kruskal_list.append(np.nan)
+                eigvals_list.append([])
+                eigvecs_list.append([])
 
         # find the optimum k from the minimum kruskal
-        ind_opt = np.argmin(np.array(kruskal_list))
+        ind_opt = np.nanargmin(np.array(kruskal_list))
 
         self.k_near = k_list[ind_opt]
-    
         eigvals_opt = eigvals_list[ind_opt]
         eigvecs_opt = eigvecs_list[ind_opt]
+
+        # # reordering eigvels, eigvecs and snapshots according descending magnitude order
+        # idx_sort = np.argsort(-np.abs(eigvals_opt))
+        # print(idx_sort)
+        # eigvals_opt = eigvals_opt[idx_sort]
+        # eigvecs_opt = eigvecs_opt[:,idx_sort]
+        # self.snapshots = snapshots[idx_sort,:]
 
         # relative information content to cut additional modes if unnecessary
         d = 1
         for d in range(1, eigvals_opt.shape[0]+1):
             ric = np.sum(eigvals_opt[:d]**2) / np.sum(eigvals_opt**2)
             if ric > delta:
+                self.isomap_degree = d     # latent space dimensionality
                 break
+        self.isomap_degree = eigvals_opt.shape[0]
+        self.snapshots = snapshots
 
-        self.isomap_degree = d
         self.eigvals = eigvals_opt[:self.isomap_degree]                        # (d,) array
         self.eigvecs = eigvecs_opt[:,:self.isomap_degree]                      # (M,d) array
         self.latent  = (self.eigvecs @ np.diag(np.sqrt(self.eigvals))).T       # (d, M) array
@@ -99,25 +120,17 @@ class ISOMAPPCE():
 
         if not hasattr(self, 'latent'):
             raise KeyError('You to first do the ISOMAP!')
-        
-        # generating a list of pce
-        self.pces = [copy.deepcopy(PCE(**self.pce_parameters)) for i in range(self.isomap_degree)]
-        
-        # training a PCE for each component of the latent variable (each one having ndata samples)
-        for i_pce, pce in enumerate(self.pces):
-            z_train = self.latent[i_pce,:]
-            pce.compute_coeffs(X, z_train, method=method, weights=weights)
+    
+        # computing the PCE coefficients on the latent space basis
+        self.pce.compute_coeffs(X, self.latent.T, method=method, weights=weights)
 
     def moments(self, mc_samples=500):
-
-        pred_samples = np.zeros((mc_samples, self.noutputs))
 
         # sampling the random inputs X from the standard distributions
         X_samples = self._sample_x(n_samples=mc_samples)
 
-        for smp in range(mc_samples):
-
-            pred_samples[smp,:] = self.predict(X_samples[smp,:]) 
+        # Isomap prediction for the samples set
+        pred_samples = self.predict(X_samples) 
 
         mean = np.mean(pred_samples, axis=0)
         var  = np.var( pred_samples, axis=0)
@@ -127,26 +140,29 @@ class ISOMAPPCE():
     def predict(self, X):
         '''
         ROM prediction from random input sample
+        X is a (nsamples, dim) numpy.array of inputs
+        y is a (nsamples, noutputs) numpy.array of predictions
         '''
 
         if X.ndim == 1:
             X = X[np.newaxis,:]
-        elif X.ndim > 1:
-            if X.shape[1] > 1:
-                raise ValueError('You can predict at the moment one X sample at time!')
+
+        nsamples, _ = X.shape
+
+        Y_pred = np.zeros((nsamples, self.noutputs))#np.repeat(self.Y_mean[np.newaxis,:], repeats=nsamples, axis=0)
+
+        for smp in range(nsamples):
 
         # predicting the latent variable through PCE 
-        z_pred = np.zeros(self.isomap_degree)
-        for i_pce, pce in enumerate(self.pces):
-            z_pred[i_pce] = pce.predict(X)
+            z_pred = self.pce.predict(np.atleast_2d(X[smp,:]))
 
-        # finding the optimal weight of the training latent variables closest to the prediction
-        w_opt, neigh_indices = self._backmapping(z_pred)
+            # finding the optimal weight of the training latent variables closest to the prediction
+            w_opt, neigh_indices = self._backmapping(z_pred)
 
-        # prediction as weighted linear linear combination of the near snaps 
-        y_pred = self.snapshots[neigh_indices, :].T @ w_opt
+            # prediction as weighted linear linear combination of the near snaps 
+            Y_pred[smp,:] += self.snapshots[neigh_indices, :].T @ w_opt 
 
-        return y_pred
+        return Y_pred
     
     def _sample_x(self, n_samples):
         '''
@@ -216,7 +232,7 @@ class ISOMAPPCE():
         '''
         Metric to evaluate the optimum number k of nearest neighbors
         '''
-        num = np.sum((D - pdist(Z_tilde, metric='euclidean')**2))
+        num = np.sum( ( D - squareform( pdist(Z_tilde, metric='euclidean') ) )**2 )
         den = np.sum(D**2)
         return np.sqrt(num/den)
     
@@ -226,48 +242,55 @@ class ISOMAPPCE():
         optimal weights and the indexes of the k-nearest neighbors among the snapshots
         '''
 
-        if z_pred.ndim < 2:
-            z_pred = z_pred[np.newaxis,:]
+        # if z_pred.ndim < 2:
+        #     z_pred = z_pred[np.newaxis,:]
 
         nn = NearestNeighbors(n_neighbors=self.k_near, 
                               metric='euclidean', 
                               algorithm='auto')
         nn.fit(self.latent.T)
-        neigh_indices = np.squeeze(nn.kneighbors(z_pred, return_distance=False))
+        neigh_indices = np.squeeze( nn.kneighbors( np.atleast_2d(z_pred), return_distance=False) )
 
         neighs = self.latent[:, neigh_indices]
 
-        def backmap_obj(w, eps=0.01, gamma=4.0):
+        # regularization coeffs
+        c = np.zeros(self.k_near)
+        for i in range(self.k_near):
+            c[i] = np.linalg.norm(z_pred - neighs[:,i], 2.0)
+        c = 0.01 * (c / np.amax(c))**4.0
 
-            obj = np.linalg.norm(z_pred - neighs.dot(w),2.0)**2
-
-            c = np.zeros(self.k_near)
-            for i in range(c.shape):
-                c[i] = eps * ( np.linalg.norm(z_pred - neighs[:,i])**2 / 
-                               np.amax( np.linalg.norm( np.repeat(z_pred[:,np.newaxis],self.k_near,axis=1) - neighs, axis=0)**2 ) )**gamma
-                
-            obj += c.dot(w**2)
-            
+        def backmap_obj(w):
+            obj = np.linalg.norm(z_pred - neighs.dot(w), 2.0)**2 + c.dot(w**2)
             return obj
         
+        def backmap_obj_grad(w):
+            grad = - 2 * np.sum(z_pred - neighs.dot(w)) * np.sum(neighs, axis=0) + 2 * c
+            return grad
+        
         def backmap_con(w):
-            return np.eye(w.size).dot(w) - 1
+            return np.sum(w) - 1
+        
+        def backmap_con_grad(w):
+            return np.ones(w.shape[0])
 
-        weight_bnd = Bounds(lb=[-2]*self.k_near, ub=[2]*self.k_near)
+        weight_bnd = [(-2,2)] * self.k_near
 
         w_list = []
         f_list = []
-        for start in range(10):
+        for _ in range(10):
             w0 = np.random.uniform(-2, 2, self.k_near)
-            opt_results = minimize(backmap_obj, 
-                                   w0,
-                                   method='SLSQP',
-                                   bounds=weight_bnd,
-                                   constraints=[{'type': 'eq', 'fun': backmap_con}],
-                                   tol=1e-7,
-                                   options={'maxiter':1000}) 
-            w_list.append(opt_results.x)
-            f_list.append(opt_results.fun)
+            w_opt_i, f_opt_i, _, _, _ = fmin_slsqp(backmap_obj, 
+                                                w0,
+                                                bounds=weight_bnd,
+                                                f_eqcons=backmap_con,
+                                                fprime=backmap_obj_grad,
+                                                fprime_eqcons=backmap_con_grad,
+                                                acc=1e-8,
+                                                iter=10000,
+                                                disp=0,
+                                                full_output=True) 
+            w_list.append(w_opt_i)
+            f_list.append(f_opt_i)
 
         w_opt = np.array(w_list)[np.argmin(np.array(f_list)),:]
 
